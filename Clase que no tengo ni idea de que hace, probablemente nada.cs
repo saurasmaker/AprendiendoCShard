9014e6fd663d051f7977474399784d21be6dfc8c
ShiroHaru99 <48929700+ShiroHaru99@users.noreply.github.com>
2020-08-16 20:27:05 +0200

    Esto mismo

---
+++ Clase que no tengo ni idea de que hace, probablemente nada.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AprendiendoCShard
+{
+    class Clase_que_no_tengo_ni_idea_de_que_hace__probablemente_nada
+    {
+    }
+}
