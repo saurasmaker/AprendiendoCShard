4bcdb2b2b69bcfe464a5cea697c36ca6f1eb7350
ShiroHaru99 <48929700+ShiroHaru99@users.noreply.github.com>
2020-08-16 23:12:32 +0200

    Creacion de Po3
    Po3 es una clase básica creada para probar el merge con Poligono

---
+++ Po3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AprendiendoCShard
+{
+    class Po3
+    {
+        public int lado;
+
+        public Po3() { }
+
+        public Po3(int lado) {
+            this.lado = lado;
+        }
+
+        int DobleLado() {
+            return (this.lado * 2);
+        }
+
+        static ChorroPalabras()
+        {
+            Console.WriteLine("Esto es un chorro de palabras que uso para definir una estatic porque tengo que practicar, pero no confundir con el Puñal de Statikk que eso hace cosas y esto no\nComprendes?");
+        }
+    }
+}
