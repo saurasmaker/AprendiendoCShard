3f135d52947621ad841f4725c6ce622abcc5151c
Iván <48927047+saurasmaker@users.noreply.github.com>
2020-08-16 22:58:35 +0200

    Mucha mierda que tienes que aprender
    Estudialo con detalle. Es un poco confuso, pero en cuanto lo pilles, entenderás POO.

--- Program.cs
+++ Program.cs
@@ -10,10 +10,20 @@ namespace AprendiendoCShard
             Console.WriteLine("Guardamos en memoria un T2.");
 
             
-            T2 triangulo2 = new T2(); //Guardamos en memoria un objeto T2
+            T2 triangulo2 = new T2(); //Guardamos en memoria un objeto T2      
             triangulo2.lados = 3;
             triangulo2.vertices = 3;
             Console.WriteLine(triangulo2.SumaLadoVertice());
+
+            T2.MiNombreEs();
+
+            T2 triangulo2y2 = new T2(2, 3, 3);
+            Console.WriteLine(triangulo2y2.SumaLadoVertice());
+
+            //Metemos un T2 dentro de otro T2
+            triangulo2y2.t2 = triangulo2;
+            Console.WriteLine(triangulo2y2.t2.SumaLadoVertice());
+
 
         }
     }
--- T2.cs
+++ T2.cs
@@ -8,14 +8,32 @@ namespace AprendiendoCShard
     {
 
         public int lados, vertices, longitudHipotenusa;
+        public T2 t2;
 
         //Método Constructor. Los constructores tienen como nombre el nombre de la clase y no tienen tipo de variable a devolver.
         public T2()
-        {
-
+        { 
+        
         }
+
+        //Contructor sobrecargado. Ahora puedes declarar un T2 con dos tipos de contructores distintos
+        public T2(int lados, int vertices, int longitudHipotenusa)
+        {
+            this.lados = lados;
+            this.vertices = vertices;
+            this.longitudHipotenusa = longitudHipotenusa;
+        }
+
+        
         public int SumaLadoVertice() {
             return (this.lados + this.vertices); //this.lados significa que "lados" pertenece a la variable que hemos declarado arriba y no a cualquier otra variable declarada en otro lugar.
+        }
+
+        public static void MiNombreEs()
+        {
+            Console.WriteLine("Mi nombre es T2");
+
+            return;
         }
 
     }
