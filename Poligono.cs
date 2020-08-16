a316bbb069253e084504add576deb69e530a7b1d
Iván <48927047+saurasmaker@users.noreply.github.com>
2020-08-16 23:14:00 +0200

    Creación de un esquema de clase tomando de ejemplo Polígono

--- Poligono.cs
+++ Poligono.cs
@@ -4,7 +4,56 @@ using System.Text;
 
 namespace AprendiendoCShard
 {
+    /*
+     * Clase padre de todos los polígonos. Cualquier polígono (triangulo, cuadrado, pentagono, etc) deberá heredar
+     * de esta clase.
+     */
     class Poligono
     {
+        //Atributos
+        private int lados, vertices;
+
+        //Getters & Setters
+        public int Lados { get => lados; set => lados = value; }
+        public int Vertices { get => vertices; set => vertices = value; }
+
+        //Constructores
+        public Poligono()
+        {
+
+        }
+
+        public Poligono(int lados, int vertices)
+        {
+            this.lados = lados;
+            this.vertices = vertices;
+        }
+
+        
+        //Métodos
+        public void MostrarLados()
+        {
+            Console.WriteLine("Tengo {0} lados.", this.lados);
+
+            return;
+        }
+
+        public void MostrarVertices()
+        {
+            Console.WriteLine("Tengo {0} vertices.", this.vertices);
+
+            return;
+        }
+
+
+
+        //Métodos Státicos
+        public static void MostrarNombre()
+        {
+
+            Console.WriteLine("Soy un polígono.");
+
+            return;
+        }
     }
 }
