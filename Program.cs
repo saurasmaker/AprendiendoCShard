89fafec29243651a61ff3ae620c453ba4a985547
ShiroHaru99 <48929700+ShiroHaru99@users.noreply.github.com>
2020-08-20 18:06:30 +0200

    Crear XML
    He creado un código básico de pruebas para ir trasteando con XML.
    De momento solo genera archivos XML con datos que te pide.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml;
 
 namespace AprendiendoCShard
 {
@@ -113,7 +114,8 @@ namespace AprendiendoCShard
     {
         static void Main(string[] args)
         {
-            /*Entradas de Usuario:*/
+            /*
+              /*Entradas de Usuario:*/
             // Mostrar por pantalla
             /* Console.WriteLine("Mostrar cosas"); //es como el printf
 
@@ -123,9 +125,9 @@ namespace AprendiendoCShard
             Console.WriteLine(nombre); //de esta forma mostramos por pantalla el valor de nombre, que es una string (un char[] vamos).*/
 
             //Ejemplo de uso:
-            Console.WriteLine("Nombre?");
+            /*Console.WriteLine("Nombre?");
             string introducir = Console.ReadLine();
-            Console.WriteLine("Hola " + introducir);
+            Console.WriteLine("Hola " + introducir);*/
 
             //Tipos de datos:
             /*
@@ -145,16 +147,16 @@ namespace AprendiendoCShard
              */
 
             //Operaciones y cosas:
-            var a = 1;
+            /*var a = 1;
             var b = 2;
-            var c = a + b;
+            var c = a + b;*/
             //Esto es una suma, usando var te quitas el problema de la compatibilidad de datos entre int, float y esas mierdas.
-            c = a - b;
-            c = a / b;
-            c = a * b;
-            c = a % b; //Esto es el módulo, es decir, el resto de la división entera.
-            //Se puede hacer lo tipico del i++//i--//++i//--i que es igual que en C y todos sabemos para que sirve.
-            //Hay una variación que es poniendo i+=X, donde x es un valor, y cambia en ese valor, válido para las combinaciones previas.
+            /* c = a - b;
+             c = a / b;
+             c = a * b;
+             c = a % b;*/ //Esto es el módulo, es decir, el resto de la división entera.
+                          //Se puede hacer lo tipico del i++//i--//++i//--i que es igual que en C y todos sabemos para que sirve.
+                          //Hay una variación que es poniendo i+=X, donde x es un valor, y cambia en ese valor, válido para las combinaciones previas.
 
             /*
              Existen diferentes funciones que son útiles:
@@ -169,9 +171,9 @@ namespace AprendiendoCShard
              */
 
             //Numero Aleatorio
-            Random numero = new Random();
+            //Random numero = new Random();
 
-            var aleatorio = numero.Next(1, 11); //Genera un numero al azar entre 1 y 10, excluye el ultimo numero.
+            //var aleatorio = numero.Next(1, 11); //Genera un numero al azar entre 1 y 10, excluye el ultimo numero.
 
             //Condicionales y Bucles:
             //Operadores relacionales [< > <= >= == !=] [Menor, Mayor, Menor o Igual, Mayor o Igual, Igual, Distinto]
@@ -232,12 +234,12 @@ namespace AprendiendoCShard
 
             //LISTAS
             //Arriba debe ir declarado using System.Collections.Generic
-            List<int> listaDeNumeros = new List<int>();
+            //List<int> listaDeNumeros = new List<int>();
 
             /*listaDeNumeros.Add(X)*///Esto sirve para añadir al final un numero, pero la lista puede ser de cualquier cosa, no solo numeros.
-            // listaDeNumeros.AddRange(Array);
-            // Se puede crear una lista a raiz de un array: List<int> listaDeNumeros = new List<int>(array);
-            //Tambien se puede: List<int> listaDeNumeros = new List<int>(new int[] {1, 2, 3, 4}); de esta forma defines que quieres un array pero no defines el array como tal
+                                     // listaDeNumeros.AddRange(Array);
+                                     // Se puede crear una lista a raiz de un array: List<int> listaDeNumeros = new List<int>(array);
+                                     //Tambien se puede: List<int> listaDeNumeros = new List<int>(new int[] {1, 2, 3, 4}); de esta forma defines que quieres un array pero no defines el array como tal
 
             //listaDeNumeros.Insert(0, 14); esta funcion inserta el 14 en la posicion 0 de la lista SUSTITUYENDO lo que hubiera ahi.
             //listaDeNumeros.Remove(14); esta funcion busca los 14 de la lista y los borra para siempre.
@@ -247,12 +249,12 @@ namespace AprendiendoCShard
             /* 
              Las estructuras se definen fuera del Main (como siempre), con la sintaxis struct nombre{COSAS}
                 Se crea una estructura así -> [Nombre de la Estructura definida] [Nombre de la variable] = new [Nombre de la Estructura definida]*/
-            Estructura prueba = new Estructura();//Ejemplo de creacion de una Estructura definida arriba.
-            prueba.Entero = 0;//Se accede a sus atributos poniendo un puntito porque son PUBLICOS.
-            //Las Estructuras tienen un CONSTRUCTOR de ESTRUCTURAS que se define (opcionalmente) dentro de la propia estructura
-            Estructura prueba2 = new Estructura(12, "Segunda", 1.2f);
+            /* Estructura prueba = new Estructura();//Ejemplo de creacion de una Estructura definida arriba.
+             prueba.Entero = 0;//Se accede a sus atributos poniendo un puntito porque son PUBLICOS.
+             //Las Estructuras tienen un CONSTRUCTOR de ESTRUCTURAS que se define (opcionalmente) dentro de la propia estructura
+             Estructura prueba2 = new Estructura(12, "Segunda", 1.2f);*/
             //APARTE las estructuras pueden tener métodos internos propios que no son el constructor.
-            prueba2.Info();//Llamamos al método.
+            /*prueba2.Info();//Llamamos al método.
 
             Estructura[] SuperEstructura =
             {
@@ -266,29 +268,128 @@ namespace AprendiendoCShard
             foreach(Estructura dato in datos)
             {
                 dato.Info(); //Con este bucle recorremos la lista entera.
-            }
+            }*/
             //CLASES
             //Arriba esta guardada la estructura de un animal.
-            Animal sPerro = new Animal("Perro");
+            /*Animal sPerro = new Animal("Perro");
             Animal sPerroCopia = sPerro;
 
             sPerro.nombre = "Gato";
             
             sPerro.info();
-            sPerroCopia.info();
+            sPerroCopia.info();*/
             //Ejemplo de Estructuras, ahora se usa lo mismo con las clases para comprobar que no son objetos independientes.
-            claseAnimal cPerro = new claseAnimal("Perro");
+            /*claseAnimal cPerro = new claseAnimal("Perro");
             claseAnimal cPerroCopia = cPerro;
 
             cPerro.nombre = "Gato";
 
             cPerro.info();
-            cPerroCopia.info();//La diferencia entre las estructuras y las clases es que las estructuras son valores staticas mientras que las clases hacen referencia en memoria y heredan de otras, todos los cambios.
+            cPerroCopia.info();*///La diferencia entre las estructuras y las clases es que las estructuras son valores staticas mientras que las clases hacen referencia en memoria y heredan de otras, todos los cambios.
 
-            AnimalSonido PERRO = new AnimalSonido();
+            /*AnimalSonido PERRO = new AnimalSonido();
             PERRO.nombre = "Gustavo";
             PERRO.PESO = 2.00;
-            PERRO.SONIDO = "Woff";           
+            PERRO.SONIDO = "Woff";*/
+
+            //XML
+            /*XmlDocument Documento = new XmlDocument();
+            XmlElement raiz = Documento.CreateElement("Raiz");
+            Documento.AppendChild(raiz);
+
+            XmlElement libro = Documento.CreateElement("Libro");
+            raiz.AppendChild(libro);
+
+            XmlElement titulo = Documento.CreateElement("Titulo");
+            titulo.AppendChild(Documento.CreateTextNode("Esto es un titulo"));*/
+
+            XmlDocument Bosses = new XmlDocument(); //Se crea un superelemento llamado Bosses
+            XmlElement raiz = Bosses.CreateElement("Raiz"); //Creo un elemento raiz, hijo de Bosses que contendrá todos los demas elementos.
+            Bosses.AppendChild(raiz);
+            XmlElement Dinamax = Bosses.CreateElement("Dinamax"); //Creo el tipo Dinamax, y le hago hijo de raiz
+            raiz.AppendChild(Dinamax);
+            XmlElement Gigamax = Bosses.CreateElement("Gigamax"); //Creo el tipo Gigamax, y le hago hijo de raiz
+            raiz.AppendChild(Gigamax);
+
+            XmlElement NombreDina = Bosses.CreateElement("Nombre"); //Creo y declaro el Nombre de los Dinamax, debo hacerlo asi porque como va a ser hijo de Dinamax no valdría para los Gigamax.
+            Dinamax.AppendChild(NombreDina);
+            XmlElement CRdina = Bosses.CreateElement("CR"); //Igual que con nombre pero con CR
+            Dinamax.AppendChild(CRdina);
+            XmlElement NombreGiga = Bosses.CreateElement("Nombre"); //Creo y declaro el nombre de los Gigamax, porque no puedo usar la variable igual de los dinamax.
+            Gigamax.AppendChild(NombreGiga);
+            XmlElement CRgiga = Bosses.CreateElement("CR"); //Igual que antes
+            Gigamax.AppendChild(CRgiga);
+
+            int dina = 0; //estas variables estan para detectar si es la primera vez que se accede al tipo, es decir, para rellenar el espacio declarado arriba y que no se quede vacío.
+            int giga = 0;
+
+
+            Console.WriteLine("¿Quieres crear un Boss?\nIntroduce Si para empezar o cualquier botón para salir");
+            string Condicion = Console.ReadLine();
+            while (Condicion == "Si" || Condicion == "si" || Condicion == "SI") //He puesto por si acaso varias formas de escribir Si, para que a la gente no le den embolias
+            {
+                Console.WriteLine("¿Es Gigamax o Dinamax?");
+                Condicion = Console.ReadLine();
+                if (Condicion == "Gigamax" || Condicion == "gigamax" || Condicion == "giga" || Condicion == "Giga" || Condicion == "GIGA"|| Condicion == "GIGAMAX") //Same here que con si
+                {
+                    if (giga == 0) //Aqui mira si es la primera vez que se crea un Gigamax
+                    {
+                        Console.WriteLine("\n\nIntroduce el nombre: ");
+                        NombreGiga.AppendChild(Bosses.CreateTextNode(Console.ReadLine()));
+                        Console.WriteLine("\nIntroduce su CR: ");
+                        CRgiga.AppendChild(Bosses.CreateTextNode(Console.ReadLine()));
+                        giga++;
+                    }
+                    else
+                    {
+                        Gigamax = Bosses.CreateElement("Gigamax");
+                        raiz.AppendChild(Gigamax);
+                        Console.WriteLine("\n\nIntroduce el nombre: ");
+                        NombreGiga = Bosses.CreateElement("Nombre");
+                        Gigamax.AppendChild(NombreGiga);
+                        NombreGiga.AppendChild(Bosses.CreateTextNode(Console.ReadLine()));
+                        Console.WriteLine("\nIntroduce su CR: ");
+                        CRgiga = Bosses.CreateElement("CR");
+                        Gigamax.AppendChild(CRgiga);
+                        CRgiga.AppendChild(Bosses.CreateTextNode(Console.ReadLine()));
+                    }
+                    
+                }
+                else if (Condicion == "Dinamax" || Condicion == "dinamax" || Condicion == "dina" || Condicion == "Dina" || Condicion == "DINA" || Condicion == "DINAMAX") //Same here que con si
+                {
+                    if (dina == 0) //Aqui mira si es la primera vez que se crea un Dinamax
+                    {
+                        Console.WriteLine("\n\nIntroduce el nombre: ");
+                        NombreDina.AppendChild(Bosses.CreateTextNode(Console.ReadLine()));
+                        Console.WriteLine("\nIntroduce su CR: ");
+                        CRdina.AppendChild(Bosses.CreateTextNode(Console.ReadLine()));
+                        dina++;
+                    }
+                    else
+                    {
+                        Dinamax = Bosses.CreateElement("Dinamax");
+                        raiz.AppendChild(Dinamax);
+                        Console.WriteLine("\n\nIntroduce el nombre: ");
+                        NombreDina = Bosses.CreateElement("Nombre");
+                        Dinamax.AppendChild(NombreDina);
+                        NombreDina.AppendChild(Bosses.CreateTextNode(Console.ReadLine()));
+                        Console.WriteLine("\nIntroduce su CR: ");
+                        CRdina = Bosses.CreateElement("CR");
+                        Dinamax.AppendChild(CRdina);
+                        CRdina.AppendChild(Bosses.CreateTextNode(Console.ReadLine()));
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("No has utilizado un tipo especificado");
+                }
+                Console.WriteLine("¿Quieres continuar o salir?\nIntroduce Si para empezar o cualquier botón para salir");
+                Condicion = Console.ReadLine();
+            }
+
+            Bosses.Save("d:\\xml\\Bosses.xml"); //Y aquí se guarda el archivo xml, es probable que tengas que editar esto para que funcione.
+            
 
         }
     }
