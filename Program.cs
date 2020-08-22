e6b057b55f1067b5a6c1efbe0912bd1a0a12024d
Iván <48927047+saurasmaker@users.noreply.github.com>
2020-08-22 12:58:49 +0200

    Pendiente: Cambiar a aplicacion de Windows Forms

--- Program.cs
+++ Program.cs
@@ -1,17 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Xml;
 using AprendiendoCSharp.Pokemon;
+using System.Windows;
 
 namespace AprendiendoCSharp
 {     
+
     class Program
     {
-        static void Main(string[] args)
+
+        [STAThread]
+        static void Main()
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
             OPokemon pokemon = Pokedex.LoadPokemonFromXML("2");
             pokemon.Show();
 
