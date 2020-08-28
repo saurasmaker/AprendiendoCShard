787d95a9732643a43e4b6f7d46c98a9050fe7b29
ShiroHaru99 <48929700+ShiroHaru99@users.noreply.github.com>
2020-08-28 17:36:00 +0200

    Carpeta Server
    He creado el Server

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+using System.Net.Sockets;
+
 
 namespace AprendiendoCShard
 {
@@ -6,8 +14,29 @@ namespace AprendiendoCShard
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Esto es Develop");
+            Thread HILO = new Thread(()=>GO("Cosita","Pasan"));
+            HILO.Start();
+            HILO.Join();
+            Thread.Sleep(2000);
+            for(int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("x");
+            }
+        }
+
+        static void Metodo(object o)
+        {
+            Console.WriteLine(o.ToString());
+            for (int i= 0; i<1000; i++)
+            {
+                Console.WriteLine("y");
+            }
+        }
+
+        static void GO(string s1, string s2)
+        {
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
         }
     }
 }
---
+++ Sockets/Server.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace AprendiendoCShard.Sockets
+{
+    class Server
+    {
+
+        public void conection()
+        {
+            Socket firstSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+    }
+}
