4c938ed19872e17f1c68e3ed581b103811e151a5
ShiroHaru99 <48929700+ShiroHaru99@users.noreply.github.com>
2020-08-28 17:38:27 +0200

    fix

--- Sockets/Server.cs
+++ Sockets/Server.cs
@@ -15,6 +15,26 @@ namespace AprendiendoCShard.Sockets
         public void conection()
         {
             Socket firstSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint direction = new IPEndPoint(IPAddress.Parse("localhost"), 1414);
+
+
+            firstSocket.Bind(direction);
+            firstSocket.Listen(5);
+
+            Console.WriteLine("Escuchando...");
+
+            Socket Listen = firstSocket.Accept();
+            Console.WriteLine("Conectado con éxito");
+
+            byte[] byRec = new byte[255];
+            int a = Listen.Receive(byRec, 0, byRec.Length, 0);
+
+            Array.Resize(ref byRec, a);
+            Console.WriteLine("Cliente dice: "+ Encoding.Default.GetString(byRec)); //Mostramos lo recibido
+            firstSocket.Close();
+            Console.WriteLine("Presione cualquier tecla para salir");
+            Console.ReadKey();
         }
     }
 }
